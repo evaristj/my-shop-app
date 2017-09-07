0a321754478441b68d6563c7b2695ba26e844244
Evarist <evaristjaumet@gmail.com>
2017-09-07 20:44:15 +0200

    primer commit

---
+++ src/app/item-list/item-list.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Item } from './item-models';
+import { ITEMS } from './mocks';
+
+
+@Component({
+  selector: 'app-item-list',
+  templateUrl: './item-list.component.html',
+  styleUrls: ['./item-list.component.css']
+})
+export class ItemListComponent implements OnInit {
+  myItems: Item[];
+  fin = 'Give Feedback';
+  link = 'pincha estos links, amigo';
+
+  constructor() { }
+
+  ngOnInit() {
+    this.myItems = ITEMS;
+  }
+  totalItems() {
+  /* otra forma de recorrer un elemento de un array y sumarlo
+    let sum = 0;
+    for (let item of this.myItems){
+      sum += item.stock;
+    }
+    return sum;
+    */
+    /*
+    totalItems(){
+    return this.myItems.reduce(function (prev, current) { return prev + current.stock; }, 0);
+    }
+    totalItems(){
+    return this.myItems.reduce( (prev, current) => prev + current.stock, 0);
+    }
+    */
+    let total = 0;
+    this.myItems.forEach(item => {
+      total += item.stock;
+    });
+    return total;
+  }
+
+}
---
+++ src/app/item-list/mocks.ts
@@ -0,0 +1,23 @@
+import { Item } from './item-models';
+
+export const ITEMS: Item[] = [{
+    'id': 1,
+    'name': 'Item name',
+    'description': 'these item is the best',
+    'stock': 5,
+    'price': 15.45
+  },
+  {
+    'id': 2,
+    'name': 'Another item name',
+    'description': 'these item is the best second',
+    'stock': 8,
+    'price': 12.99
+  },
+  {
+    'id': 3,
+    'name': 'Another item name more',
+    'description': 'these item is the best therd',
+    'stock': 0,
+    'price': 9.99
+  }];
