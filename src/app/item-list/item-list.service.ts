e80425f8de8bc353413ae69760f2c45be6dc8364
Evarist <evaristjaumet@gmail.com>
2017-09-11 20:50:39 +0200

    ultimo commit

--- src/app/item-list/item-list.service.ts
+++ src/app/item-list/item-list.service.ts
@@ -1,7 +1,7 @@
 import { Item } from './item-models';
 import { ITEMS } from './mocks';
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 
 @Injectable()
@@ -14,4 +14,22 @@ export class ItemListService {
         .map((res: Response) => res.json())
         .catch( (error: any) => Observable.throw(error.json().error || 'Server error') );
     }
+    addItem(item: Item): Observable<Item[]> {
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions({ headers: headers });
+
+        return this.http.post('http://localhost:3000/item-list', item, options)
+                        .map((res: Response) => res.json())
+                        .catch((error: any) => Observable.throw(error.json().error || 'Server error'))
+    }
+
+    updateItem(item: Item): Observable<Item[]> {
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions({ headers: headers });
+
+        return this.http.post('http://localhost:3000/'+ item.id, item, options)
+                        .map((res: Response) => res.json())
+                        .catch((error: any) => Observable.throw(error.json().error || 'Server error'))
+    }
 }
+
